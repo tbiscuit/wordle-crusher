1716c5f08d9f2757f4e3685cc0a456c2c902a5e0
Tyson Jensen <twjensen@gmail.com>
2022-02-25 14:11:33 -0600

    Add possibles.
    Also this is the only commit with the crazy default path munging that resulted from not checking for typos.

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use wordl_crush::{Solver};
 
 use std::fs;
 
-fn load_allowed(fname: &str) -> Vec<String> {
+fn load_list_from_file(fname: &str) -> Vec<String> {
     let raw_text_or_err = fs::read_to_string(fname);
     let mut o: Vec<String> = Vec::new();
     if let Ok(raw_text) = raw_text_or_err {
@@ -18,24 +18,60 @@ fn load_allowed(fname: &str) -> Vec<String> {
     o
 }
 
-fn allowed_guesses_fname(args: Vec<String>) -> String {
-    let mut arg_iter = args.iter();
-    loop {
-        let arg = arg_iter.next();
-        if let Some(a) = arg {
-            if a == "--file" {
-                let file_arg = arg_iter.next();
-                if let Some(f) = file_arg {
-                    return f.to_string();
+#[macro_export]
+macro_rules! path_up {
+    ($deflt: expr) => {
+        if true {
+            let def_root = std::env::current_dir();
+            if let Ok(base_path) = def_root {
+                // Note: This could fail for a sufficiently annoying OS.
+                let maybe_path = base_path.to_str();
+                if let Some(def_path) = maybe_path {
+                    let mut out_path: String = String::from(def_path);
+                    out_path.push('/');
+                    out_path.push_str($deflt);
+                    out_path
                 } else {
-                    break;
+                    String::from($deflt)
                 }
+            } else {
+                String::from($deflt)
             }
         } else {
-            break;
+            String::from($deflt)
         }
     }
-    String::from("data/wordle-allowed-guesses.txt")
+}
+
+
+#[macro_export]
+macro_rules! load_wlist {
+    ($arg_iter: expr, $flag: expr, $deflt: expr) => {
+        loop {
+            let arg = $arg_iter.next();
+            if let Some(a) = arg {
+                if a == $flag {
+                    let file_arg = $arg_iter.next();
+                    if let Some(f) = file_arg {
+                        return f.to_string();
+                    } else {
+                        return String::from($deflt);
+                    }
+                }
+            } else {
+                return String::from($deflt);
+            }
+        }
+    }
+}
+
+fn allowed_guesses_fname(args: Vec<String>) -> String {
+    let mut arg_iter = args.iter();
+    load_wlist!(arg_iter, "--allowed", "data/wordle-allowed-guesses.txt");
+}
+fn possible_guesses_fname(args: Vec<String>) -> String {
+    let mut arg_iter = args.iter();
+    load_wlist!(arg_iter, "--possible", "data/wordle-possible-solutions.txt");
 }
 
 use std::env;
@@ -45,6 +81,14 @@ fn main() {
     // into a Vec like it's 1999.
     let args: Vec<String> = env::args().collect();
 
-    let wordl_allowed = load_allowed(&allowed_guesses_fname(args));
-    println!("Loaded {} wordl allowed words", wordl_allowed.len());
+    let wordl_allowed = load_list_from_file(&allowed_guesses_fname(args.clone()));
+    let wordl_possible = load_list_from_file(&possible_guesses_fname(args));
+    println!("Loaded {} wordl allowed words, with {} possible solutions",
+             wordl_allowed.len(), wordl_possible.len());
+    let try_words = wordl_possible.clone();
+    for word in try_words {
+        let solver = Solver::create(wordl_allowed.clone(), wordl_possible.clone());
+        let guesses = solver.solve(word.clone());
+        println!("used {} guesses to solve {}", guesses.len(), word);
+    }
 }
--- src/solver.rs
+++ src/solver.rs
@@ -2,10 +2,18 @@
 
 pub struct Solver {
     allowed: Vec<String>,
+    possible: Vec<String>,
 }
 
 impl Solver {
-    pub fn solve(_secret: String) -> Vec<String> {
+    pub fn create(a: Vec<String>, p: Vec<String>) -> Solver{
+        Solver {
+            allowed: a,
+            possible: p,
+        }
+    }
+
+    pub fn solve(&self, _secret: String) -> Vec<String> {
         let o: Vec<String> = Vec::new();
         o
     }
